9241721b4e37daaadcd62c95f143aa3e2c32031f
Renan Cesar <cesar.renan@hotmail.com>
2023-01-26 20:53:51 -0300

    Estilizando header

--- src/components/header/Header.Styled.ts
+++ src/components/header/Header.Styled.ts
@@ -2,6 +2,41 @@ import styled from "styled-components";
 import { Box } from '@mui/material'
 
 export const BoxSC = styled(Box)`
-background-color: var(--azul);
-height: 101px;
+    background-color: var(--azul);
+    height: 101px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 60px;
+
+    .img-container {
+        display: flex;
+        align-items: flex-end;
+        font-size: 20px;
+        font-weight: 300;
+        color: var(--branco);
+        gap: 10px;
+        user-select: none;
+    }
+
+    .btn-container {
+
+        .btn {
+            background-color: var(--branco);
+            width: 90px;
+            height: 45px;
+            border-radius: 8px;
+            display: flex;
+            justify-content: space-between;
+            padding: 20px;
+        }
+        
+        & p {
+            color: var(--preto);
+            font-size: 18px;
+            font-weight: 700;
+        }
+    }
+
 `
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,25 @@
 import { BoxSC } from "./Header.Styled"
+import { Box, Typography, Button } from '@mui/material'
+import Logo from '../../assets/MKS.png'
+import Car from '../../assets/car.png'
 
 export const Header = () => {
-  return (
-    <BoxSC>
-      <h1>Home</h1>
-    </BoxSC>
-  )
+    return (
+        <BoxSC>
+            <Box className='img-container'>
+                <img src={Logo}/>
+                <Typography>
+                    Sistemas
+                </Typography>
+            </Box>
+            <Box className='btn-container'>
+                <Button className="btn">
+                    <img src={Car}/>
+                    <Typography component={'p'}>
+                        0
+                    </Typography>
+                </Button>
+            </Box>
+        </BoxSC>
+    )
 }
