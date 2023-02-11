9cd0aa8a4fe361c2a351fac757d8514166525e83
Renan Cesar <cesar.renan@hotmail.com>
2023-02-11 20:05:14 -0300

    Retirando código desnecessário

--- src/components/drawer/Drawer.Styled.ts
+++ src/components/drawer/Drawer.Styled.ts
@@ -47,25 +47,5 @@ export const BoxItem = styled(Box)`
     width: 100%;
     color: var(--preto);
     overflow-y: auto;  
-    padding: 20px;
-
-& ::-webkit-scrollbar {
-  width: 10px;
-}
-
-/* Track */
-& ::-webkit-scrollbar-track {
-  background: #f1f1f1;
-}
-
-/* Handle */
-& ::-webkit-scrollbar-thumb {
-  background: #888;
-}
-
-/* Handle on hover */
-& ::-webkit-scrollbar-thumb:hover {
-  background: #555;
-}
-    
+    padding: 20px;    
 `
