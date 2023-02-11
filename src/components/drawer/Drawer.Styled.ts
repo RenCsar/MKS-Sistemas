7f0b6e9de8e6b2fa48f396ab4b7a93ceee5754f9
Renan Cesar <cesar.renan@hotmail.com>
2023-02-11 20:04:20 -0300

    Estlizando o scroll bar

--- src/components/drawer/Drawer.Styled.ts
+++ src/components/drawer/Drawer.Styled.ts
@@ -48,4 +48,24 @@ export const BoxItem = styled(Box)`
     color: var(--preto);
     overflow-y: auto;  
     padding: 20px;
+
+& ::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+& ::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+& ::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+& ::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+    
 `
