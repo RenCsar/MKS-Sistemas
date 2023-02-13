faa44d8c99422b900b087007d4164d5d9841c125
Renan Cesar <cesar.renan@hotmail.com>
2023-02-13 00:00:12 -0300

    Retirando link da antiga api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  // baseURL: 'https://mks-challenge-api-frontend.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC'
   baseURL: 'https://tech-shopping-api-production.up.railway.app/produtos'
 })
