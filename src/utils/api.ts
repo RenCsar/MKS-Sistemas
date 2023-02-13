0b37faff99185aa40a974a4d9cf39031b9785139
Renan Cesar <cesar.renan@hotmail.com>
2023-02-12 23:27:40 -0300

    Trocando url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: 'https://mks-challenge-api-frontend.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC'
+  // baseURL: 'https://mks-challenge-api-frontend.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC'
+  baseURL: 'https://tech-shopping-api-production.up.railway.app/produtos'
 })
